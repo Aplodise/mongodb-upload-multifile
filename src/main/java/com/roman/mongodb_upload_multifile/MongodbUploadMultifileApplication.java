68bf7551856e7ba4f8b270e6126717ecd8ae36c9
Aplodise <ronmacdonald228@gmail.com>
2024-09-07 13:07:37 +0200

    Initial commit

---
+++ src/main/java/com/roman/mongodb_upload_multifile/MongodbUploadMultifileApplication.java
@@ -0,0 +1,13 @@
+package com.roman.mongodb_upload_multifile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbUploadMultifileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbUploadMultifileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roman/mongodb_upload_multifile/MongodbUploadMultifileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.mongodb_upload_multifile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbUploadMultifileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
